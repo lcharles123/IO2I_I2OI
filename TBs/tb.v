fa77182638642dd807be96fe53c137d72f8de8d2
lcharles123 <root@master>
2020-10-06 22:43:45 -0300

    arquivo unico

---
+++ TBs/tb.v
@@ -0,0 +1,28 @@
+module tb();
+
+	reg clk, rst;
+	wire [31:0] saida;
+	
+	initial
+	begin
+		#0 clk <= 0; rst <= 0;
+		#1 rst <= 1;
+	end
+	
+	always #1 clk = ~clk;
+	
+	Core core(clk, rst, saida);
+
+	initial 
+	begin
+        $dumpfile("dump.vcd"); //gerar arquivo de forma de onda, para ser usado no gtkwave
+        $dumpvars(0,tb);
+        
+        $monitor("saida: %b", saida);
+		
+		#1000 $finish;
+    end
+    
+
+endmodule
+
